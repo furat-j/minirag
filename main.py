b1ba343b1a0dcc8058b2de87ac2968af151b5ae0
Furat J <furatjarrah03@gmail.com>
2026-01-08 14:37:21 +0300

    starting the project with simple test

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 
+# app = FastAPI()
+#
+#
+# @app.get("/api/v1/")
+# async def root():
+#     return {"message": "Hello World"}
+#
+#
+# @app.get("/api/v1/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
+
+from fastapi import FastAPI
 app = FastAPI()
 
-
-@app.get("api/v1/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("api/v1/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/welcome")
+async def welcome():
+    return {
+        "message": "Hello World!"
+    }
